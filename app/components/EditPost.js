82a960b0e502c1850bb83f2fd699acd8653809dd
Shermainesng <shersmallsng@gmail.com>
2022-02-17 22:46:19 +0800

    client side validations

---
+++ app/components/EditPost.js
@@ -0,0 +1,162 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useImmerReducer } from "use-immer";
+import Page from "./Page";
+import { useParams, Link } from "react-router-dom";
+import Axios from "axios";
+import LoadingDotsIcon from "./LoadingDotsIcon";
+import StateContext from "../StateContext";
+import DispatchContext from "../DispatchContext";
+
+function EditPost() {
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
+
+  const originalState = {
+    title: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    body: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    isFetching: true,
+    isSaving: false, //when we click on that button to update the post. set this to true when user clicks on the btn
+    id: useParams().id,
+    sendCount: 0 //how many times we've tried to send an Axios request
+  };
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "fetchComplete":
+        //insert new value into title.value in our initial state
+        draft.title.value = action.value.title; //the object that user sent back to us
+        draft.body.value = action.value.body;
+        draft.isFetching = false;
+        return;
+      case "titleChange":
+        draft.title.hasErrors = false;
+        draft.title.value = action.value;
+        return;
+      case "bodyChange":
+        draft.body.hasErrors = false;
+        draft.body.value = action.value;
+        return;
+      case "submitRequest":
+        if (!draft.title.hasErrors && !draft.body.hasErrors) {
+          //only if both fields are not blank
+          draft.sendCount++;
+        }
+        return;
+      case "saveRequestStarted":
+        draft.isSaving = true;
+        return;
+      case "saveRequestFinished":
+        draft.isSaving = false;
+        return;
+      case "titleRules":
+        if (!action.value.trim()) {
+          //if there  is no value (value does not exist)
+          //ppl who just leaves spaces in the blanks is considered a blank field too
+          draft.title.hasErrors = true;
+          draft.title.message = "You must provide a title";
+        }
+        return;
+      case "bodyRules":
+        if (!action.value.trim()) {
+          //if there  is no value (value does not exist)
+          //ppl who just leaves spaces in the blanks is considered a blank field too
+          draft.body.hasErrors = true;
+          draft.body.message = "You must provide body content";
+        }
+        return;
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, originalState);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch({ type: "titleRules", value: state.title.value }); //the second dispatch won't go through if the first can't (means hasErrors=true, so can't post)
+    //this is impt so that if my field is empty but i didn't click out of it before clicking submit, the post won't go through
+    dispatch({ type: "bodyRules", value: state.body.value });
+    dispatch({ type: "submitRequest" });
+  }
+
+  useEffect(() => {
+    const ourRequest = Axios.CancelToken.source();
+    async function fetchPost() {
+      try {
+        const response = await Axios.get(`post/${state.id}`, { cancelToken: ourRequest.token });
+        // console.log(response.data);
+        // setPost(response.data); //set the array of posts as the posts variable
+        dispatch({ type: "fetchComplete", value: response.data }); //what should happen to our state when this action occurs        setIsLoading(false);
+      } catch (e) {
+        console.log("there was a problem or the request was cancelled");
+      }
+    }
+    fetchPost();
+    return () => {
+      //if i'm loading a single post but then exit to home page, error occurs. the axios request needs some time to complete, but if i click away, the following code is still being rendered. (states are still updated even if the component is no longer mounted)
+      ourRequest.cancel(); //need a cleanup function (when a component is no longer being used)
+    };
+  }, []);
+
+  useEffect(() => {
+    if (state.sendCount) {
+      dispatch({ type: "saveRequestStarted" });
+      const ourRequest = Axios.CancelToken.source();
+      async function fetchPost() {
+        try {
+          const response = await Axios.post(`post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appState.user.token }, { cancelToken: ourRequest.token });
+          dispatch({ type: "saveRequestFinished" });
+          appDispatch({ type: "flashMessage", value: "Post was updated" });
+        } catch (e) {
+          console.log("there was a problem or the request was cancelled");
+        }
+      }
+      fetchPost();
+      return () => {
+        ourRequest.cancel();
+      };
+    }
+  }, [state.sendCount]);
+
+  if (state.isFetching)
+    return (
+      <Page title="...">
+        <LoadingDotsIcon />
+      </Page>
+    );
+
+  return (
+    <Page title="Edit Post">
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <label htmlFor="post-title" className="text-muted mb-1">
+            <small>Title</small>
+          </label>
+          <input onBlur={e => dispatch({ type: "titleRules", value: e.target.value })} onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          {state.title.hasErrors && <div className="alert alert-danger small liveValidateMessage">{state.title.message}</div>}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="post-body" className="text-muted mb-1 d-block">
+            <small>Body Content</small>
+          </label>
+          <textarea onBlur={e => dispatch({ type: "bodyRules", value: e.target.value })} onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} value={state.body.value} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" />
+          {state.body.hasErrors && <div className="alert alert-danger small liveValidateMessage">{state.body.message}</div>}
+          {/* if is true, button will be disabled */}
+        </div>
+
+        <button className="btn btn-primary" disabled={state.isSaving}>
+          Save Updates
+        </button>
+      </form>
+    </Page>
+  );
+}
+
+export default EditPost;
